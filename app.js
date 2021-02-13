ddda938d6380df6983f9edaccdb933c08ca12f5b
srnfr <stephane@reytan.fr>
2021-02-13 16:52:26 +0100

    fom Paris

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ const port = 80;
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
   res.setHeader('Content-Type', 'text/plain');
-  res.end('Hello World');
+  res.end('Hello World from Paris');
 });
 
 //server.listen(port, hostname, () => {
